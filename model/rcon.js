75f3c21f16c032cf063062080b372fbefbf823a0
Harukaze <phantomstudio299@gmail.com>
2018-07-21 14:23:59 +0700

    update

--- model/rcon.js
+++ model/rcon.js
@@ -1,7 +1,7 @@
 const Rcon = require('modern-rcon');
 
 //Rcon setup
-const rcon = new Rcon('localhost', 'tey0814463129')
+const rcon = new Rcon('localhost', 'password')
 rcon.connect()
 
 module.exports = rcon
